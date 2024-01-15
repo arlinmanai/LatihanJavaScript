645e0fa5109991d0614c9a43e28dd8ded5c2d651
unknown <arlinmanai19@gmail.com>
2024-01-15 13:51:04 +0800

    belajar operator dan fungsi js

---
+++ 1.Memulai JS/2.Operator & Fungsi/assignment.js
@@ -0,0 +1,22 @@
+let bilangan;
+
+//assignment =
+bilangan = 10;
+
+//assignment +=
+bilangan += 5;
+
+//assignment -= 7;
+
+// *=
+bilangan *= 3;
+
+// /=
+bilangan  /= 6;
+
+bilangan %= 2;
+
+biolangan **= 4;
+
+console.log(bilangan);
+
