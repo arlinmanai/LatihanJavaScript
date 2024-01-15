b7c8560521e6349e7f2c4dfade7230d46087d4dc
unknown <arlinmanai19@gmail.com>
2024-01-15 13:24:21 +0800

    Belajar Operator dan Fungsi

---
+++ 1.Memulai JS/2.Operator & Fungsi/aritmatika.js
@@ -0,0 +1,38 @@
+let bill = 10;
+let bil2 = 3;
+
+//Operator Penjumlahan 
+let jumlah;
+jumlah = bi11 + bil12;
+console.log(jumlah);
+
+//Operator Pengurangan
+let kurang;
+kurang = bil1 - bil2;
+console.log(kurang);
+
+//Operator Perkalian
+let kali;
+kali = bil1 * bill2;
+console.log(kali);
+
+//Operator (pembagian)
+let bagi;
+bagi = bil1 / bil2;
+console.log(bagi);
+
+//Exsponen (pangkat)
+let pangkat;
+pangkat = bil1 ** bil2;
+console.log(pangkat)
+
+//Moduls (Sisa Hasil Bagi)
+let moduls = bil1 % bil2;
+console.log(modulus);
+
+//Increment | Decrement
+bi11++;
+console.log(bil1);
+bil1--;
+console.log(bil2);
+
---
+++ 1.Memulai JS/variabel.js
@@ -0,0 +1,22 @@
+// deklarasi awal
+let name = "Alin"
+let usia = "16"
+const jk = "p"
+let menikah = "false";
+
+//cetak nilai awal
+console.log(name);
+console.log(usia);
+console.log(jk);
+console.log(menikah);
+
+//perubahan isi variabel
+name = "alin manay"
+usia = 16;
+jk = "l";
+
+//cetak hasil perubahan
+console.log(name);
+console.log(usia);
+console.log(jk);
+console.log(menikah);
